aec60be9937502157932392a96f69b67ba4d7a8e
Andrey Dybov <dybov@alarstudios.com>
2023-06-08 16:59:46 +0200

    Apply django auth functionality

--- geodjango/geodjango/urls.py
+++ geodjango/geodjango/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", include("interactive_points.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/login/', auth_views.LoginView.as_view(
+        template_name='registration/login.html',
+        redirect_field_name='index'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
 ]
